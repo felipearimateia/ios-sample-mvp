257c69334a042518a41acf6fc456f401cea970f6
Felipe Arimateia <felipearimateia@gmail.com>
2019-03-10 18:21:02 -0300

    Initial Commit

---
+++ Pocket Rick and Morty/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Pocket Rick and Morty
+//
+//  Created by Felipe Arimateia on 10/03/19.
+//  Copyright © 2019 Felipe Arimateia. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
